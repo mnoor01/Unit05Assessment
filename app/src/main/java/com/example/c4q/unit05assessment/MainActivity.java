fafc7b217bcc56e2e32475244be801a05ed28d6c
mnoor01 <MuhaimenNoor@ac.c4q.nyc>
2018-01-24 19:46:33 -0500

    initial commit

---
+++ app/src/main/java/com/example/c4q/unit05assessment/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.c4q.unit05assessment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+//this will be my first commit to the master branch
