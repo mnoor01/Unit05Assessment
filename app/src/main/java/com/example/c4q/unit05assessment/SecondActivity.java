575e7b88d5bb91c9948fea2be8b3c2365f99b2ff
mnoor01 <MuhaimenNoor@ac.c4q.nyc>
2018-01-27 21:29:18 -0500

    more organized code

--- app/src/main/java/com/example/c4q/unit05assessment/SecondActivity.java
+++ app/src/main/java/com/example/c4q/unit05assessment/SecondActivity.java
@@ -7,19 +7,26 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
-    private TextView infos;
+    private TextView locationView,dobView,emailView;
     private ImageView infopic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        infos=findViewById(R.id.infos);
+        locationView=findViewById(R.id.locationinfo);
+        dobView=findViewById(R.id.dobinfo);
+        emailView=findViewById(R.id.emailinfo);
         Intent intent= getIntent();
         String location= intent.getStringExtra("location");
         String email= intent.getStringExtra("email");
         String dob= intent.getStringExtra("dob");
-        infos.setText(location+" "+email+" "+dob);
+        locationView.setText(location);
+        emailView.setText(email);
+        dobView.setText(dob);
+
+
+
 
     }
 }
